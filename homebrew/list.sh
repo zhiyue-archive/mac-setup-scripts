ad042e66a8ce08d98dc6bd83d5038fa9a09036b8
zhiyue <cszhiyue@gmail.com>
2021-06-26 11:08:21 +0800

    setup normal install

--- homebrew/list.sh
+++ homebrew/list.sh
@@ -3,22 +3,22 @@
 ########### Brew Install List ###########
 brew install ansible
 brew install aria2
-brew install arp-scan
+# brew install arp-scan
 brew install arping
-brew install awk
+# brew install awk
 brew install axel
-brew install docker-completion
-brew install docker-compose-completion
-brew install docker-machine
-brew install docker-machine-completion
-brew install e2fsprogs
-brew install fd
+# brew install docker-completion
+# brew install docker-compose-completion
+# brew install docker-machine
+# brew install docker-machine-completion
+# brew install e2fsprogs
+# brew install fd
 brew install ffmpeg
 brew install fping
 brew install fzf
 brew install git
-brew install go
-brew install graphviz
+# brew install go
+# brew install graphviz
 brew install hstr
 brew install htop
 brew install hub
@@ -26,146 +26,145 @@ brew install ilmbase
 brew install imagemagick
 brew install iperf3
 brew install irssi
-brew install jakehilborn/jakehilborn/displayplacer
-brew install jansson
+# brew install jakehilborn/jakehilborn/displayplacer
+# brew install jansson
 brew install jq
-brew install kubernetes-cli
+# brew install kubernetes-cli
 brew install lastpass-cli
-brew install lepton
+# brew install lepton
 brew install libmaxminddb
 brew install libmemcached
 brew install libressl
 brew install libsmi
 brew install libssh
-brew install mackup
-brew install mas
-brew install mercurial
+# brew install mercurial
 brew install mitmproxy
 brew install mosh
 brew install mtr
-brew install nativefier
+# brew install nativefier
 brew install nmap
 brew install nnn
-brew install openjdk
+# brew install openjdk
 brew install p7zip
 brew install pipenv
 brew install pyenv-virtualenv
 brew install python@3.8
-brew install redis
+# brew install redis
 brew install restic
 brew install rsync
-brew install smartmontools
+# brew install smartmontools
 brew install speedtest-cli
 brew install sshpass
 brew install tcping
 brew install telnet
-brew install tinc
-brew install tldr
+# brew install tinc
+# brew install tldr
 brew install tmux
-brew install unar
-brew install weechat
+# brew install unar
+# brew install weechat
 brew install wget
 brew install you-get
 brew install youtube-dl
 brew install zsh-navigation-tools
 
-########### Brew Cask Install List ###########
-brew cask install adoptopenjdk8
-brew cask install aerial
-brew cask install alfred
-brew cask install android-platform-tools
-brew cask install avibrazil-rdm
-brew cask install baidunetdisk
-brew cask install bettertouchtool
-brew cask install bitbar
-brew cask install clashx
-brew cask install clean-me
-brew cask install contexts
-brew cask install dash
-brew cask install deskreen
-brew cask install discord
-brew cask install docker
-brew cask install drawio
-brew cask install dropbox
-brew cask install element
-brew cask install feishu
-brew cask install font-fira-code
-brew cask install font-fira-code-nerd-font
-brew cask install font-ibm-plex
-brew cask install font-input
-brew cask install font-iosevka
-brew cask install font-iosevka-nerd-font
-brew cask install font-iosevka-slab
-brew cask install font-iosevka-ss11
-brew cask install font-jetbrains-mono
-brew cask install font-sarasa-gothic
-brew cask install google-chrome
-brew cask install google-drive-file-stream
-brew cask install hiddenbar
-brew cask install iina
-brew cask install istat-menus
-brew cask install iterm2
-brew cask install java
-brew cask install jetbrains-toolbox
-brew cask install karabiner-elements
-brew cask install launchrocket
-brew cask install lepton
-brew cask install mellow
-brew cask install microsoft-auto-update
-brew cask install microsoft-edge
-brew cask install notion
-brew cask install obsidian
-brew cask install omnidisksweeper
-brew cask install onedrive
-brew cask install openwebstart
-brew cask install postman
-brew cask install proxifier
-brew cask install qq
-brew cask install riot
-brew cask install shadowsocksx-ng
-brew cask install slack
-brew cask install smcfancontrol
-brew cask install sogouinput
-brew cask install sourcetree
-brew cask install surge
-brew cask install switchresx
-brew cask install syncthing
-brew cask install telegram
-brew cask install the-unarchiver
-brew cask install tigervnc-viewer
-brew cask install tuntap
-brew cask install typora
-brew cask install visual-studio-code
-brew cask install wechat
-brew cask install wireshark
-brew cask install zotero
+# brew install --cask adoptopenjdk8
+# brew install --cask aerial
+brew install --cask alfred
+# brew install --cask android-platform-tools
+# brew install --cask avibrazil-rdm
+brew install --cask baidunetdisk
+# brew install --cask bettertouchtool
+# brew install --cask bitbar
+brew install --cask clashx
+brew install --cask clean-me
+# brew install --cask contexts
+# brew install --cask dash
+# brew install --cask deskreen
+brew install --cask discord
+# brew install --cask docker
+brew install --cask drawio
+brew install --cask dropbox
+# brew install --cask element
+brew install --cask feishu
+brew install --cask font-fira-code
+brew install --cask font-fira-code-nerd-font
+brew install --cask font-ibm-plex
+brew install --cask font-input
+brew install --cask font-iosevka
+brew install --cask font-iosevka-nerd-font
+brew install --cask font-iosevka-slab
+brew install --cask font-iosevka-ss11
+brew install --cask font-jetbrains-mono
+brew install --cask font-sarasa-gothic
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask hiddenbar
+brew install --cask iina
+brew install --cask istat-menus
+brew install --cask iterm2
+# brew install --cask java
+brew install --cask jetbrains-toolbox
+brew install --cask karabiner-elements
+# brew install --cask launchrocket
+# brew install --cask lepton
+# brew install --cask mellow
+brew install --cask microsoft-auto-update
+brew install --cask microsoft-edge
+brew install --cask notion
+# brew install --cask obsidian
+# brew install --cask omnidisksweeper
+brew install --cask onedrive
+# brew install --cask openwebstart
+brew install --cask postman
+brew install --cask proxifier
+# brew install --cask qq
+# brew install --cask riot
+# brew install --cask shadowsocksx-ng
+brew install --cask slack
+# brew install --cask smcfancontrol
+brew install --cask sogouinput
+brew install --cask sourcetree
+# brew install --cask surge
+# brew install --cask switchresx
+# brew install --cask syncthing
+brew install --cask telegram
+brew install --cask the-unarchiver
+# brew install --cask tigervnc-viewer
+brew install --cask tuntap
+brew install --cask typora
+brew install --cask visual-studio-code
+brew install --cask wechat
+# brew install --cask wireshark
+# brew install --cask zotero
+brew install --cask wechatwork
+brew install --cask microsoft-office
 
 ########### Mac App Store Install List ###########
-mas install 1037779758  # Best Trace (1.33)
-mas install 409183694  # Keynote (10.3.5)
-mas install 1403919533  # MWeb (3.4.4)
-mas install 1454805783  # Picsee (1.2.2)
-mas install 1278508951  # Trello (2.11.7)
+# mas install 1037779758  # Best Trace (1.33)
+# mas install 409183694  # Keynote (10.3.5)
+# mas install 1403919533  # MWeb (3.4.4)
+# mas install 1454805783  # Picsee (1.2.2)
+# mas install 1278508951  # Trello (2.11.7)
 mas install 1059334054  # 截图 (2.3.0)
 mas install 966085870  # TickTick (3.7.85)
 mas install 409907375  # Remote Desktop (3.9.3)
-mas install 682658836  # GarageBand (10.3.5)
+# mas install 682658836  # GarageBand (10.3.5)
 mas install 1314842898  # Kantu (2.5.2)
-mas install 503039729  # Fotor Photo Editor (3.6.2)
-mas install 973134470  # Be Focused (2.0.2)
-mas install 1451685025  # WireGuard (0.0.20191105)
-mas install 1453504509  # Dynamic Wallpaper (5.0)
-mas install 1527428847  # Snip (1.4)
-mas install 409203825  # Numbers (10.3.5)
+# mas install 503039729  # Fotor Photo Editor (3.6.2)
+# mas install 973134470  # Be Focused (2.0.2)
+# mas install 1451685025  # WireGuard (0.0.20191105)
+# mas install 1453504509  # Dynamic Wallpaper (5.0)
+# mas install 1527428847  # Snip (1.4)
+# mas install 409203825  # Numbers (10.3.5)
 mas install 985367838  # Microsoft Outlook (16.46)
-mas install 409201541  # Pages (10.3.5)
-mas install 1006087419  # SnippetsLab (1.9.3)
+# mas install 409201541  # Pages (10.3.5)
+# mas install 1006087419  # SnippetsLab (1.9.3)
 mas install 595615424  # QQ音乐 (7.4.0)
-mas install 408981434  # iMovie (10.2.1)
-mas install 1221250572  # Xnip (2.2.1)
+# mas install 408981434  # iMovie (10.2.1)
+# mas install 1221250572  # Xnip (2.2.1)
 mas install 1274495053  # Microsoft To Do (2.33)
-mas install 1475387142  # Tailscale (0.100.94)
-mas install 836505650  # Battery Monitor (2.4.4)
-mas install 1485844094  # iShot (1.8.1)
+# mas install 1475387142  # Tailscale (0.100.94)
+# mas install 836505650  # Battery Monitor (2.4.4)
+# mas install 1485844094  # iShot (1.8.1)
 
 echo "And done..."
