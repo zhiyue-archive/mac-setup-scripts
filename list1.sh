dc8d0ee70ff5169c35aa768b08a5bec48f4ed4b8
zhiyue <cszhiyue@gmail.com>
2021-06-26 11:29:53 +0800

    fixed init.sh

---
+++ list1.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+########### Brew Install List ###########
+brew install ansible
+brew install aria2
+brew install arp-scan
+brew install arping
+brew install awk
+brew install axel
+brew install docker-completion
+brew install docker-compose-completion
+brew install docker-machine
+brew install docker-machine-completion
+brew install e2fsprogs
+brew install fd
+brew install ffmpeg
+brew install fping
+brew install fzf
+brew install gh
+brew install git
+brew install go
+brew install graphviz
+brew install hstr
+brew install htop
+brew install hub
+brew install ilmbase
+brew install imagemagick
+brew install iperf3
+brew install irssi
+brew install jakehilborn/jakehilborn/displayplacer
+brew install jansson
+brew install jq
+brew install kubernetes-cli
+brew install lastpass-cli
+brew install lepton
+brew install libmaxminddb
+brew install libmemcached
+brew install libressl
+brew install libsmi
+brew install libssh
+brew install mackup
+brew install mas
+brew install mercurial
+brew install mitmproxy
+brew install mosh
+brew install mtr
+brew install nativefier
+brew install nmap
+brew install nnn
+brew install openjdk
+brew install p7zip
+brew install pipenv
+brew install pyenv-virtualenv
+brew install python@3.8
+brew install redis
+brew install restic
+brew install rsync
+brew install smartmontools
+brew install speedtest-cli
+brew install sshpass
+brew install tcping
+brew install telnet
+brew install tinc
+brew install tldr
+brew install tmux
+brew install unar
+brew install weechat
+brew install wget
+brew install you-get
+brew install youtube-dl
+brew install zsh-navigation-tools
+
+########### Brew Cask Install List ###########
+
+########### Mac App Store Install List ###########
+mas install 1037779758  # Best Trace (1.33)
+mas install 409183694  # Keynote (10.3.5)
+mas install 1403919533  # MWeb (3.4.4)
+mas install 1454805783  # Picsee (1.2.2)
+mas install 1278508951  # Trello (2.11.7)
+mas install 1059334054  # 截图 (2.3.0)
+mas install 966085870  # TickTick (3.7.85)
+mas install 409907375  # Remote Desktop (3.9.3)
+mas install 682658836  # GarageBand (10.3.5)
+mas install 1314842898  # Kantu (2.5.2)
+mas install 503039729  # Fotor Photo Editor (3.6.2)
+mas install 973134470  # Be Focused (2.0.2)
+mas install 1451685025  # WireGuard (0.0.20191105)
+mas install 1453504509  # Dynamic Wallpaper (5.0)
+mas install 1527428847  # Snip (1.4)
+mas install 409203825  # Numbers (10.3.5)
+mas install 985367838  # Microsoft Outlook (16.46)
+mas install 409201541  # Pages (10.3.5)
+mas install 1006087419  # SnippetsLab (1.9.3)
+mas install 595615424  # QQ音乐 (7.4.0)
+mas install 408981434  # iMovie (10.2.1)
+mas install 1221250572  # Xnip (2.2.1)
+mas install 1274495053  # Microsoft To Do (2.33)
+mas install 1475387142  # Tailscale (0.100.94)
+mas install 836505650  # Battery Monitor (2.4.4)
+mas install 1485844094  # iShot (1.8.1)
+
+echo "And done..."
