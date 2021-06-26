d02bb284461937ffc327e3302dd2afddd40d5f58
zhiyue <cszhiyue@gmail.com>
2021-06-26 11:08:03 +0800

    update homebrew install method

--- homebrew/export.sh
+++ homebrew/export.sh
@@ -5,10 +5,10 @@
 echo -e '#!/bin/bash\n' > list.sh
 
 echo -e '########### Brew Install List ###########' >> list.sh
-brew leaves | sed 's/^/brew install /' >> list.sh &&
+brew leaves | sed 's/^/brew install /' >> list1.sh &&
 
 echo -e '\n########### Brew Cask Install List ###########' >> list.sh &&
-brew list --cask | sed 's/^/brew cask install /' >> list.sh &&
+brew list --cask | sed 's/^/brew install --cask /' >> list1.sh &&
 
 echo -e '\n########### Mac App Store Install List ###########' >> list.sh &&
 mas list | sed 's/\ /\ \ #\ /' | sed 's/^/mas install /' >> list.sh
